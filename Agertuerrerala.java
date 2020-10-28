3d2388d8c8014a01273d226cb25c49afde5abe61
Herrera04 <73583137+Herrera04@users.noreply.github.com>
2020-10-28 10:11:12 +0100

    Add files via upload

---
+++ Agertuerrerala.java
@@ -0,0 +1,21 @@
+package EB1;
+import java.util.Scanner;
+public class Agertuerrerala {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Ingurunea
+				double N=0;
+				Scanner teklatua=new Scanner(System.in);
+						
+				// Algoritmoa
+				System.out.println("Sartu zenbaki bat");
+				N=teklatua.nextDouble();
+				System.out.printf("Zuk sartutako zenbakia %.2f da" ,N );
+						
+				teklatua.close();
+		
+	}
+
+}
